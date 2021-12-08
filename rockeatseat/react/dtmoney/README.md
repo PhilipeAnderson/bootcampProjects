00311f9df228da5579e4c40f6623120a4da26fb8
philipe_kraken <philipeacampos@hotmail.com>
2021-12-07 21:52:03 -0300

    Configured styled components with GlobalStyle

---
+++ rockeatseat/react/dtmoney/README.md
@@ -0,0 +1,4 @@
+# Settings of project
+
+Figma:
+https://www.figma.com/file/0xmu9mj2TJYoIOubBFWsk5/dtmoney-Ignite-(Copy)?node-id=0%3A1
--- rockeatseat/react/dtmoney/src/App.tsx
+++ rockeatseat/react/dtmoney/src/App.tsx
@@ -1,8 +1,11 @@
+import { GlobalStyle } from "./styles/global";
+
 
 export function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <GlobalStyle />
     </div>
   );
 }
