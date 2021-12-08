326167295f37e0d23150ebfae1d6219e155dd15a
philipe_kraken <philipeacampos@hotmail.com>
2021-12-08 18:52:48 -0300

    Three boxes created at the top of the application

---
+++ rockeatseat/react/dtmoney/src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import logo from '../../assets/logo.svg';
+import { Container, Content } from './styles';
+
+
+export function Header(){
+  return(
+    <Container>
+      <Content>
+        <img src={ logo } alt="dtmoney" />
+        <button type="button">
+          Nova Transação
+        </button>
+      </Content>  
+    </Container>
+  )
+}
