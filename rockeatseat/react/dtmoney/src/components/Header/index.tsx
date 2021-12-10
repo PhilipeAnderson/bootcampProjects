399c9fc6923787e063dc42ec47786acb4b979166
philipe_kraken <philipeacampos@hotmail.com>
2021-12-10 16:33:14 -0300

    modal configured

--- rockeatseat/react/dtmoney/src/components/Header/index.tsx
+++ rockeatseat/react/dtmoney/src/components/Header/index.tsx
@@ -1,13 +1,20 @@
 import logo from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
+interface HeaderProps{
+  onOpenNewTransactionModal: () => void;
+}
 
-export function Header(){
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps){
+
+
+
   return(
     <Container>
       <Content>
         <img src={ logo } alt="dtmoney" />
-        <button type="button">
+        <button type="button" onClick={onOpenNewTransactionModal}>
           Nova Transação
         </button>
       </Content>  
