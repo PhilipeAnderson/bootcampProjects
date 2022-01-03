e0831f839cd671d4107bb81464897cc3595bdf99
philipe_kraken <philipeacampos@hotmail.com>
2022-01-03 18:21:00 -0300

    finished the modal styling

--- rocketseat/react/dtmoney/src/components/NewTransactionModal/index.tsx
+++ rocketseat/react/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import Modal from 'react-modal';
-import close from '../../assets/close.svg';
-import { Container } from './styles';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import closeImg from '../../assets/close.svg';
+import { Container, TransactionTypeContainer, RadioBox } from './styles';
 
 interface NewTransactionModalProps{
   isOpen: boolean;
@@ -9,6 +12,10 @@ interface NewTransactionModalProps{
 
 
 export function NewTransactionModal({ isOpen, onRequestClose }:NewTransactionModalProps){
+
+
+  const [type, setType] = useState('deposit')
+
   return(
     <Modal 
         isOpen={isOpen} 
@@ -18,7 +25,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }:NewTransactionMod
       >
 
       <button type="button" onClick={onRequestClose} className="react-modal-close">
-        <img src={close} alt="Fechar Modal" />
+        <img src={ closeImg } alt="Fechar Modal" />
       </button>
 
 
@@ -34,7 +41,25 @@ export function NewTransactionModal({ isOpen, onRequestClose }:NewTransactionMod
           placeholder="Valor"
         />
 
-        
+        <TransactionTypeContainer>
+
+          <RadioBox 
+            type="button" 
+            onClick={ () => setType('deposit') }
+            isActive={ type === 'deposit' }
+            >
+            <img src={ incomeImg } alt="Entradas" />
+          </RadioBox>
+
+          <RadioBox 
+            type="button" 
+            onClick={ () => setType('withdraw') }
+            isActive={ type === 'withdraw' }
+          >
+            <img src={ outcomeImg } alt="Saidas" />
+          </RadioBox>
+
+        </TransactionTypeContainer>
 
 
 
