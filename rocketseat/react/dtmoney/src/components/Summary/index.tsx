447fd606305d9c87cfff2bdf866fecfe174a53e8
philipe_kraken <philipeacampos@hotmail.com>
2022-01-04 11:19:26 -0300

    The component summary is ready

--- rocketseat/react/dtmoney/src/components/Summary/index.tsx
+++ rocketseat/react/dtmoney/src/components/Summary/index.tsx
@@ -9,7 +9,22 @@ export function Summary(){
 
   const { transactions } = useContext(TransactionsContext)
 
-  console.log(transactions)
+  const summary = transactions.reduce((acc, transaction) => {
+    if(transaction.type === 'deposit') {
+      acc.deposits += transaction.amount;
+      acc.total += transaction.amount;
+    } else {
+      acc.withdraws += transaction.amount;
+      acc.total -= transaction.amount;
+    }
+    
+    return acc;
+
+  }, {
+    deposits: 0,
+    withdraws: 0,
+    total: 0
+  })
 
   return(
     <Container>
@@ -20,7 +35,10 @@ export function Summary(){
           <img src={ income } alt="Entradas" />
         </header>
         <strong>
-          R$:1.000,00
+          { new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.deposits) }
         </strong>
       </div>
 
@@ -30,7 +48,10 @@ export function Summary(){
           <img src={ outcome } alt="Saídas" />
         </header>
         <strong>
-          - R$:500,00
+          - { new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.withdraws) }
         </strong>
       </div>
 
@@ -40,7 +61,10 @@ export function Summary(){
           <img src={ total } alt="Total" />
         </header>
         <strong>
-          R$:500,00
+          { new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.total ) }
         </strong>
       </div>
 
