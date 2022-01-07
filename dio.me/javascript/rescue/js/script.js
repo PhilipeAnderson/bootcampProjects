bdf7b8c9d06ab84bec0b62b89ab5092120a74434
philipe_kraken <philipeacampos@hotmail.com>
2022-01-07 18:29:53 -0300

    background, enemy, friend animated

--- dio.me/javascript/rescue/js/script.js
+++ dio.me/javascript/rescue/js/script.js
@@ -1,9 +1,24 @@
 function start() { 
 
 	$("#inicio").hide();
-	$("#fundoGame").append("<div id='jogador' class='animal'></div>");
-	$("#fundoGame").append("<div id='inimigo1'></div>");
+	$("#fundoGame").append("<div id='jogador' class='anima1'></div>");
+	$("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
 	$("#fundoGame").append("<div id='inimigo2'></div>");
-	$("#fundoGame").append("<div id='amigo'></div>");
+	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+
+	var jogo = {}
+	//Game Loop
+	jogo.timer = setInterval(loop,30);
+	
+	function loop() {
+		movefundo();
+		
+	}
+
+	function movefundo() {
+		esquerda = parseInt($("#fundoGame").css("background-position"));
+		$("#fundoGame").css("background-position",esquerda-1);
+		
+		}
 
 }
