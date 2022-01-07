8c055be210d1dd00c4c4758c5440eb9e8b781aa0
philipe_kraken <philipeacampos@hotmail.com>
2022-01-07 17:59:20 -0300

    Started helicopter and rescue game for DIO Javascript with Jquey bootcamp

---
+++ dio.me/javascript/rescue/README.md
@@ -0,0 +1,3 @@
+Download dos arquivos do jogo
+
+https://www.dropbox.com/s/17010wb608q4olu/JogosHTML5.zip?dl=0
---
+++ dio.me/javascript/rescue/js/script.js
@@ -0,0 +1,9 @@
+function start() { 
+
+	$("#inicio").hide();
+	$("#fundoGame").append("<div id='jogador' class='animal'></div>");
+	$("#fundoGame").append("<div id='inimigo1'></div>");
+	$("#fundoGame").append("<div id='inimigo2'></div>");
+	$("#fundoGame").append("<div id='amigo'></div>");
+
+}
