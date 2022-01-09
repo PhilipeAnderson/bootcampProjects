1e6a014b9b48b2ef2151b59ff763a2efb798d40e
philipe_kraken <philipeacampos@hotmail.com>
2022-01-09 20:46:51 -0300

    Finished style Header

--- rocketseat/react/ignews/src/pages/_document.tsx
+++ rocketseat/react/ignews/src/pages/_document.tsx
@@ -7,6 +7,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&family=Poppins:wght@100;400;600&family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         </Head>
         <body>
           <Main />
