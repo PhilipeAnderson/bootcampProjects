5f164fdbafe8f601e93d4d7ea15da3fcc8516b19
philipe_kraken <philipeacampos@hotmail.com>
2022-01-10 17:40:17 -0300

    Finished page rate

--- devsuperior/springreact/projectMovieFrontendBackend/dsmovie/frontend/src/pages/Main/index.tsx
+++ devsuperior/springreact/projectMovieFrontendBackend/dsmovie/frontend/src/pages/Main/index.tsx
@@ -3,8 +3,10 @@ import styles from './styles.module.scss';
 export function Main() {
   return(
     <section className={styles.container}>
-      <a className={styles.title} href="/">Lista dos Filmes</a>
-
+      <h1>Welcome to our app!</h1>
+      <a href="rate/1" >Filme 1</a>
+      <a href="rate/2" >Filem 2</a>
+      <a href="rate/3" >Filme 3</a>
     </section>
   )
 }
--- devsuperior/springreact/projectMovieFrontendBackend/dsmovie/frontend/src/pages/Rate/index.tsx
+++ devsuperior/springreact/projectMovieFrontendBackend/dsmovie/frontend/src/pages/Rate/index.tsx
@@ -1,7 +1,44 @@
 import styles from './styles.module.scss';
 
+
 export function Rate() {
+
+  const movie = {
+    id: 1,
+    image: "https://www.themoviedb.org/t/p/original/hv7o3VgfsairBoQFAawgaQ4cR1m.jpg",
+    title: "The Witcher",
+    count: 2,
+    score: 4.5
+  };
+
+
+
   return(
-    <h1 className={styles.container}>Rate</h1>
+    <div className={styles.container} >
+    <div className={styles.content} >
+      <img src={movie.image} alt={movie.title} />
+      <h3>{movie.title}</h3>
+      <form>
+            <div>
+                <label htmlFor="email">Informe seu email</label>
+                <input type="email" className="form-control" id="email" />
+            </div>
+            <div className="form-group">
+                <label htmlFor="score">Informe sua avaliação</label>
+                <select className="form-control" id="score">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                </select>
+            </div>
+            
+            <button type="submit" className="btn btn-primary">Salvar</button>
+            
+        </form >
+        <button className="btn btn-danger mt-3">Cancelar</button>
+    </div >
+</div >
   )
 }
