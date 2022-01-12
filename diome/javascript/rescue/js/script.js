49cad22442113fada62e724dbbc2ecc919e0e92c
philipe_kraken <philipeacampos@hotmail.com>
2022-01-12 14:44:30 -0300

    Add function move up and move down of player

--- diome/javascript/rescue/js/script.js
+++ diome/javascript/rescue/js/script.js
@@ -1,4 +1,4 @@
-function start() { 
+function start() {
 
 	$("#inicio").hide();
 	$("#fundoGame").append("<div id='jogador' class='anima1'></div>");
@@ -7,18 +7,59 @@ function start() {
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
 	var jogo = {}
+	var TECLA = {
+		W: 87,
+		S: 83,
+		D: 68
+	}
+
+	jogo.pressionou = [];
+
+	//Verifica se o usuario pressionou alguma tecla	
+
+	$(document).keydown(function (e) {
+		jogo.pressionou[e.which] = true;
+	});
+
+
+	$(document).keyup(function (e) {
+		jogo.pressionou[e.which] = false;
+	});
+
 	//Game Loop
-	jogo.timer = setInterval(loop,30);
-	
+	jogo.timer = setInterval(loop, 30);
+
 	function loop() {
 		movefundo();
-		
+		movejogador();
+
 	}
 
 	function movefundo() {
 		esquerda = parseInt($("#fundoGame").css("background-position"));
-		$("#fundoGame").css("background-position",esquerda-1);
-		
+		$("#fundoGame").css("background-position", esquerda - 1);
+
+	}
+
+	function movejogador() {
+
+		if (jogo.pressionou[TECLA.W]) {
+			var topo = parseInt($("#jogador").css("top"));
+			$("#jogador").css("top", topo - 10);
+
 		}
+
+		if (jogo.pressionou[TECLA.S]) {
+
+			var topo = parseInt($("#jogador").css("top"));
+			$("#jogador").css("top", topo + 10);
+		}
+
+		if (jogo.pressionou[TECLA.D]) {
+
+				
+		}
+
+	}
 
 }
