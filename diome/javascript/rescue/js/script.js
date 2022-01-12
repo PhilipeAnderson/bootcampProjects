f37509ad5017e81eba1163e96a92f51a6db38a15
philipe_kraken <philipeacampos@hotmail.com>
2022-01-12 15:06:28 -0300

    Moving enemys and frind on the game

--- diome/javascript/rescue/js/script.js
+++ diome/javascript/rescue/js/script.js
@@ -7,6 +7,8 @@ function start() {
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
 	var jogo = {}
+	var velocidade = 5;
+	var posicaoY = parseInt(Math.random() * 334);
 	var TECLA = {
 		W: 87,
 		S: 83,
@@ -32,6 +34,9 @@ function start() {
 	function loop() {
 		movefundo();
 		movejogador();
+		moveinimigo1();
+		moveinimigo2();
+		moveamigo();
 
 	}
 
@@ -47,19 +52,64 @@ function start() {
 			var topo = parseInt($("#jogador").css("top"));
 			$("#jogador").css("top", topo - 10);
 
+			if (topo <= 0) {
+				$("#jogador").css("top", topo + 10);
+			}
+
 		}
 
 		if (jogo.pressionou[TECLA.S]) {
 
 			var topo = parseInt($("#jogador").css("top"));
 			$("#jogador").css("top", topo + 10);
+
+			if (topo >= 434) {
+				$("#jogador").css("top", topo - 10);
+			}
 		}
 
 		if (jogo.pressionou[TECLA.D]) {
 
-				
-		}
 
+		}
+	}
+
+	function moveinimigo1() {
+
+		posicaoX = parseInt($("#inimigo1").css("left"));
+		$("#inimigo1").css("left", posicaoX - velocidade);
+		$("#inimigo1").css("top", posicaoY);
+
+		if (posicaoX <= 0) {
+			posicaoY = parseInt(Math.random() * 334);
+			$("#inimigo1").css("left", 694);
+			$("#inimigo1").css("top", posicaoY);
+
+		}
+	}
+
+	function moveinimigo2() {
+		posicaoX = parseInt($("#inimigo2").css("left"));
+		$("#inimigo2").css("left", posicaoX - 3);
+
+		if (posicaoX <= 0) {
+
+			$("#inimigo2").css("left", 775);
+
+		}
+	}
+
+	function moveamigo() {
+	
+		posicaoX = parseInt($("#amigo").css("left"));
+		$("#amigo").css("left",posicaoX+1);
+					
+			if (posicaoX>906) {
+				
+			$("#amigo").css("left",0);
+						
+			}
+	
 	}
 
 }
