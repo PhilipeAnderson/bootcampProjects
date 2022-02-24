528c5f29e36dbeb7ff2bc4d62876e20bb5a5a9cd
philipe_kraken <philipeacampos@hotmail.com>
2022-02-24 14:55:39 -0300

    Added typescript in project

---
+++ voluntary/reiki/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import { Container } from '../styles/global';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Reiki Online</title>
+      </Head>
+      <Container>
+        <h1>Hello</h1>
+      </Container>
+    </>
+  )
+}
--- voluntary/reiki/src/styles/global.ts
+++ voluntary/reiki/src/styles/global.ts
@@ -5,5 +5,5 @@ export const Container = styled.div`
   padding: 0;
   width: 100%;
   height: 100%;
-  background-color: red;
+  font-family: Poppins;
 `;
