d5e08d2d04a308b26697dde3194663cbb3eb800c
philipe_kraken <philipeacampos@hotmail.com>
2022-04-18 15:52:03 -0300

    Refactored structure from archives

--- voluntary/reiki/src/routes.js
+++ voluntary/reiki/src/routes.js
@@ -1,13 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 
-import { Home } from './pages';
-import { Reiki } from './pages/reiki'
+import { Home } from './pages/Home';
+import { Reiki } from './pages/Reiki'
 
 export function ManagerRoutes() {
   return(
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/reiki" element={<Reiki />} />
+      <Route path="/Reiki" element={<Reiki />} />
     </Routes>  
   )
 }
