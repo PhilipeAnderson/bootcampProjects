e0856d859fad32d7359a451a82473608e827beb6
philipe_kraken <philipeacampos@hotmail.com>
2022-05-01 17:26:57 -0300

    First elements with ReactNative

---
+++ rocketseat/reactNative/myskills/src/pages/Home/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { 
+  Platform,
+  StyleSheet, 
+  View, 
+  Text, 
+  TextInput,
+  TouchableOpacity
+} from 'react-native';
+
+export const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}> Hy Philipe </Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="New Skill"
+        placeholderTextColor='#555555'
+      />
+
+      <TouchableOpacity style={styles.button} activeOpacity={0.7} >
+        <Text style={styles.buttonText}>Add</Text>
+      </TouchableOpacity>
+
+      <Text style={[styles.title, { marginTop: 50 }]}>
+        My Skills
+      </Text>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingHorizontal: 50,
+    paddingVertical: 70
+  },
+  title: {
+    color: '#FFFFFF',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  input: {
+    backgroundColor: '#1F1E25',
+    color: '#FFFFFF',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7
+  },
+  button: {
+    backgroundColor: '#A370F7',
+    padding: 15,
+    borderRadius:7,
+    alignItems: 'center',
+    marginTop: 20
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 17,
+    fontWeight: 'bold',
+
+  }
+})
