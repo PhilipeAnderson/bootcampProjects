ab00d8aa8124f5ef854df7a5523a1e35f3021808
philipe_kraken <philipeacampos@hotmail.com>
2022-05-03 23:45:20 -0300

    Object FromEntries and FormData

--- studies/formswithreact/src/App.js
+++ studies/formswithreact/src/App.js
@@ -1,7 +1,31 @@
+import React from 'react';
+
 function App() {
+
+  const handleInputChange = (e) => {
+    console.log('*** handleInputChange', e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+
+    console.log('*** handleSubmit', data)
+
+  }
+
   return (
-    <h1>Hy</h1>
-  );
+    <>
+    <h1>Formulários com React</h1>
+      <form onSubmit={ handleSubmit }>
+        <input type="text" name="email" placeholder="email" onChange={ handleInputChange }/>
+        <button type="submit">Enviar</button>
+      </form>
+
+      <p>Dados</p>
+    </>
+  )
 }
 
 export default App;
