f319747734b36838d4210bb64507249c1dd93d95
philipe_kraken <philipeacampos@hotmail.com>
2022-05-14 23:40:32 -0300

    Object.fromEntries

--- studies/formswithreact/src/App.js
+++ studies/formswithreact/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
 
+  const [ value, setValue ] = useState('contato@contato.com.br');
+  
   const handleInputChange = (e) => {
-    console.log('*** handleInputChange', e.target.value)
-  }
+    console.log('*** handleInputChange', e.target.value);
+    setValue(e.target.value);
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
+
     const formData = new FormData(e.target);
     const data = Object.fromEntries(formData);
 
-    console.log('*** handleSubmit', data)
+    console.log('*** handleSubmit', data);
 
-  }
+  };
 
   return (
     <>
       <h1>Formulários com React</h1>
-      <form onSubmit={ handleSubmit }>
-        <input type="text" name="email" placeholder="email" onChange={ handleInputChange }/>
-        <button type="submit">Enviar</button>
+
+      <form onSubmit={ handleSubmit } >
+        
+        <input 
+          type="text" 
+          name="email" 
+          placeholder="email"
+          value={ value } 
+          onChange={ handleInputChange } 
+        />
+
+        <button type="submit">
+          Enviar
+        </button>
+
       </form>
 
       <p>Dados</p>
